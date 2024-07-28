6d549da9b70a1f0bc58eadbd4d728d3bb3ab587f
ReSharplex <166249324+ReSharplex@users.noreply.github.com>
2024-07-28 13:01:24 +0200

    Added Converter and modified design for Calendar Items

---
+++ SharplexTimeCode/SharplexTimeCode/CustomControls/Converter/DayOfWeekConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace SharplexTimeCode.CustomControls.Converter;
+
+public class DayOfWeekConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is DayOfWeek dayOfWeek)
+        {
+            var dayOfWeekString = dayOfWeek.ToString();
+            return dayOfWeekString.Length > 3 ? dayOfWeekString.Substring(0, 3).ToUpper(culture) : dayOfWeekString;
+        }
+
+        return value;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
